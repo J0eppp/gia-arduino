1f7731370554c30d253b0858439e3c2189d18216
J0eppp <joep.van.dijk04@gmail.com>
2019-04-03 18:02:22 +0200

    Update

--- public/js/folder.js
+++ public/js/folder.js
@@ -4,4 +4,29 @@ var folder;
 $.get("/uploads/get/folder/" + reqFolder, function (data) {
   folder = data;
   document.getElementById('title').innerHTML = reqFolder;
+  var ff = document.getElementById('files-folders');
+  for (var i = 0; i < folder.length; i++) {
+    if (folder[i].split(".")[folder[i].split(".").length - 1] === "ino") {
+      var x = JSON.stringify(reqFolder + "/" + folder[i]);
+      console.log(x);
+      document.body.innerHTML += "<a href=/uploads/" + reqFolder + "/" + folder[i] + ">" + folder[i] + "</a> <button onclick='javascript:showCode(" + x + ")'>Show Code</button> <button onclick='downloadFile(" + x + ")'>Download File</button>";
+    }
+  }
 });
+
+function showCode(path) {
+  window.location = "/uploads/" + path + "/code";
+}
+
+function downloadFile(path) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,');
+  element.setAttribute('download', "downloadFile/" + path);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
--- routers/uploadsRouter.js
+++ routers/uploadsRouter.js
@@ -17,15 +17,22 @@ router.get("/get/folder/:folderName", function (req, res) {
   }
 });
 
+router.get("/:folderName/:fileName/code", function (req, res) {
+  var folderName = req.params.folderName;
+  var fileName = req.params.fileName;
+  if (fs.existsSync("./uploads/" + folderName + "/" + fileName)) {
+    var file = fs.readFileSync("./uploads/" + folderName + "/" + fileName);
+    console.log(file.toString());
+    res.render("showFile", {
+      file: file
+    });
+  } else {
+    res.send({error: "This file doesn't exists!"});
+  }
+});
+
 router.get("/:folderName?", function (req, res) {
   if (req.params.folderName) {
-    // var folderName = req.params.folderName;
-    // if (fs.existsSync("./uploads/" + folderName + "/")) {
-    //   var folder = fs.readdirSync("./uploads/" + folderName + "/");
-    //   res.send(folder);
-    // } else {
-    //   res.send(false)
-    // }
     res.render("folder")
   } else {
     var userFolders = fs.readdirSync("./uploads/");
